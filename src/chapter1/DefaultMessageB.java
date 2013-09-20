f47cef87f078e9a0ff07426564f26b18ac76abd5
fanzovino <fede.anzo.7@gmail.com>
2013-09-20 15:36:16 -0300

    asdasda

---
+++ src/chapter1/DefaultMessageB.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+public class DefaultMessageB {
+	
+	private String message = " Esto es Spring Madafacka!!!";
+	private String message = " Probando el string GIT";
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	
+
+}
